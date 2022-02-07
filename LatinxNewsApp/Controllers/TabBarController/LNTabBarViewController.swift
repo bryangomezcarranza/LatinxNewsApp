28d6e02ec949625d22125eb348115df1599d89b3
bryangomezcarranza <bryangomezcarranza@gmail.com>
2022-02-07 11:36:17 -0800

    Configure Tabbar

---
+++ LatinxNewsApp/Controllers/TabBarController/LNTabBarViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LNTabBarViewController.swift
+//  LatinxNewsApp
+//
+//  Created by Bryan Gomez on 2/7/22.
+//
+
+import UIKit
+
+class LNTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UITabBar.appearance().tintColor = .systemGray
+        UITabBar.appearance().backgroundColor = UIColor.white
+        
+        viewControllers = [createHomeVC(), createFavoritesVC()]
+        
+    }
+    
+    private func createHomeVC() -> UINavigationController {
+        
+        let homeVC = LNHomeViewController()
+        homeVC.title = "News"
+        homeVC.tabBarItem = UITabBarItem(tabBarSystemItem: .mostRecent, tag: 0)
+        
+        return UINavigationController(rootViewController: homeVC)
+    }
+    
+    private func createFavoritesVC() -> UINavigationController {
+        
+        let favortiesVC = FavoritesViewController()
+        favortiesVC.title = "Favorites"
+        favortiesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favortiesVC)
+    }
+
+}
